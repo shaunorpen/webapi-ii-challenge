3db5efae64db3141423f567edec0f23771c671fe
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-29 15:16:33 +0000

    Add styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "bulma/css/bulma.css";
 
 function App() {
   const [posts, setPosts] = React.useState([]);
   React.useEffect(() => {
-    axios.get('http://localhost:5000/api/posts')
-    .then(data => {
-      setPosts(data.data);
-    })
-    .catch(error => {
-      console.log(error.message);
-    })
-  }, [])
-  debugger
+    axios
+      .get("http://localhost:5000/api/posts")
+      .then(data => {
+        setPosts(data.data);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }, []);
   return (
-    <div className="App">
-      {
-        posts.map(post => {
-          return (
-            <div>
-              <h2>{post.title}</h2>
-              <p>{post.contents}</p>
+    <div className="section">
+      {posts.map(post => {
+        return (
+          <div className="card">
+            <div className="card-content">
+              <h2 className="title">{post.title}</h2>
             </div>
-          );
-        })
-      }
+            <p className="subtitle">{post.contents}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
